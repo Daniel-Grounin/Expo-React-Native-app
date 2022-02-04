28633939f6d80bb5fac88e006e8757d962ad608e
daniegr1 <dani.grunin@gmail.com>
2022-02-04 20:00:16 +0200

    Print submited box

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
 
 export default function App() {
+  const [enterGoal, setEnterGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) =>{
+    setEnterGoal(enteredText);
+  }
+
+  const addGoalHandler = () =>{
+    setCourseGoals(currentGoals => [...courseGoals,enterGoal]);
+  }
+
   return (
     <View style = {styles.screen}>
         <View style={styles.inputContainer}>
-          <TextInput
-           placeholder='course goal'
-           style = {styles.input}
+          <TextInput 
+            placeholder='course goal' 
+            style = {styles.input} 
+            onChangeText={goalInputHandler}
+            value={enterGoal}
           />
-          <Button title="Submit"/>
+          <Button title="Submit" onPress={addGoalHandler} />
         </View>
-        <View/>
-    </View>
+        
+        <View>
+         {courseGoals.map(goal => (
+          <View key = {goal} style = {styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+         ))}
+        </View>
+  </View>
   );
 }
 
@@ -20,17 +40,27 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
+
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: 10
   },
+
   input: {
       borderColor: 'black',
       borderWidth : 1,
       padding: 10,
       width: 200
   },
+
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 3
+  }
 
 });
